0245b822190a011c52661ce9f8f34ecd561e9873
Hasireddy <malikireddy.uma@gmail.com>
2024-10-23 12:22:24 +0200

    views and urls

--- dictionaryProject/dictionary/views.py
+++ dictionaryProject/dictionary/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_view(request):
+    return render(request,"dictionary/home.html")
+
+def search_view(request):
+    return render(request,"dictionary/search.html")
